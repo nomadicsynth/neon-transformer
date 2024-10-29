ab2492674bfcf86c25dada82b6ec65a3d56e5bf8
nomadicsynth <sam@gillham.xyz>
2024-10-30 08:09:27 +1100

    Fix echo statement. Change output dir. Add lr_scheduler_type param. Change run name. Append command to training queue.

--- train_spark_diffattention_only_expressive_baseline.sh
+++ train_spark_diffattention_only_expressive_baseline.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echpython train.py --output_dir ./outputs --overwrite_output_dir True \
+echo python train.py --output_dir ./outputs/spark-diffattn-expressive-baseline --overwrite_output_dir True \
                 --bf16 True --bf16_full_eval True \
                 --optim adamw_bnb_8bit --adam_beta1 0.9 --adam_beta2 0.98 \
-                --learning_rate 2e-4 --weight_decay 0.01 \
+                --learning_rate 2e-4 --lr_scheduler_type linear --weight_decay 0.01 \
                 --max_steps 40000 --warmup_steps 375 \
                 --logging_strategy steps --logging_steps 100 \
-                --report_to wandb --project_name neon-test --run_name spark-diffattn-baseline --watch all \
+                --report_to wandb --project_name neon-test --run_name spark-diffattn-expressive-baseline --watch all \
                 --eval_strategy steps --eval_steps 1000 --eval_on_start True \
                 --save_strategy steps --save_steps 1000 --save_total_limit 10 --load_best_model_at_end True \
                 --dataset_name HuggingFaceFW/fineweb --dataset_config_name sample-10BT \
@@ -13,4 +13,4 @@ echpython train.py --output_dir ./outputs --overwrite_output_dir True \
                 --streaming True --packing True --dataset_text_field text --dataloader_num_workers 2 \
                 --per_device_train_batch_size 8 --per_device_eval_batch_size 16 \
                 --max_seq_length 2048 --gradient_accumulation_steps 16 \
-                --seed 42 --diff_attention_mode expressive
+                --seed 42 --diff_attention_mode expressive >> training_queue.txt
