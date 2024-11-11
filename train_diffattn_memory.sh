27d353e785e1c2cb715e2dd614011849bf91727a
nomadicsynth <sam@gillham.xyz>
2024-11-11 15:38:32 +1100

    update run-name and output dir

--- train_diffattn_memory.sh
+++ train_diffattn_memory.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-python train.py --output_dir "./outputs/spark-diffattn-memory" --overwrite_output_dir True \
+python train.py --output_dir "./outputs/spark-diffattn-memory-init" --overwrite_output_dir True \
                 --bf16 True --bf16_full_eval True \
                 --optim adamw_bnb_8bit --adam_beta1 0.9 --adam_beta2 0.98 \
                 --learning_rate 3.6e-4 --lr_scheduler_type cosine --weight_decay 0.01 \
                 --max_steps 10000 --warmup_steps 500 \
                 --logging_strategy steps --logging_steps 100 \
                 --report_to wandb --watch all --wandb_log_model "end" \
-                --project_name neon-test --run_name "spark-diffattn-memory" \
+                --project_name neon-test --run_name "spark-diffattn-memory-init" \
                 --eval_strategy steps --eval_steps 1000 --eval_on_start True \
                 --save_strategy steps --save_steps 1000 --save_total_limit 10 --load_best_model_at_end True \
                 --dataset_name HuggingFaceFW/fineweb --dataset_config_name sample-10BT \
